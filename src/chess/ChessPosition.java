0a5f3a9895d35f9f7cbf996859d3e94468ed774e
Pedro Duarte <duartepls@hotmail.com>
2024-06-26 20:35:29 -0300

    Implementing possible moves of Rook

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -6,6 +6,7 @@ public class ChessPosition {
 	
 	private char column;
 	private int row;
+	
 	public ChessPosition(char column, int row) {
 		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
 			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8");
@@ -13,6 +14,7 @@ public class ChessPosition {
 		this.column = column;
 		this.row = row;
 	}
+	
 	public char getColumn() {
 		return column;
 	}
